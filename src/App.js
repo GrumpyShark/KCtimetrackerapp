04cd10577ebeecfe254da041b222b085e6f05b55
Kyrsty Cahoon <kyrsty.cahoon@intimetec.com>
2021-01-15 16:29:15 -0700

    Created HomePage Component and TodayPage Component. Added bootstrap navbar. Still need to figure out how to link it up properly.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import HomePage from "./components/HomePage";
+import TodayPage from "./components/TodayPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>This will be my time-tracking app.</p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Git
-        </a>
-      </header>
-    </div>
-  );
+  const route = window.location.pathname;
+  if (route === "/today") return <TodayPage />;
+  return <HomePage />;
 }
 
 export default App;
